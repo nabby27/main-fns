a56db5ef6125874912734ac7e014abd1cadf4c1c
nabby27 <ivancordoba77@gmail.com>
2020-12-29 11:58:17 +0100

    fix generic type on array functions

--- src/app/array/impl/arrayUtilLodash.ts
+++ src/app/array/impl/arrayUtilLodash.ts
@@ -4,15 +4,15 @@ import { ArrayUtil } from '../util/array.util.interface';
 export class ArrayUtilLodash implements ArrayUtil {
 
   first<T>(array: T[]): T {
-    return lodash.first(array);
+    return lodash.first(array) as T;
   }
 
   last<T>(array: T[]): T {
-    return lodash.last(array);
+    return lodash.last(array) as T;
   }
 
   reverse<T>(array: T[]): T[] {
-    return lodash.reverse(array);
+    return lodash.reverse(array) as T[];
   }
 
 }
