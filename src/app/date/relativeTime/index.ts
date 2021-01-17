0b9b7f3e4563f0a33ce9348ac0124f5a665b7ca7
nabby27 <ivancordoba77@gmail.com>
2021-01-17 14:25:13 +0100

    add jsdoc

--- src/app/date/relativeTime/index.ts
+++ src/app/date/relativeTime/index.ts
@@ -4,6 +4,18 @@ import { RelativeTimeFunction } from './util/relativeTime.function';
 
 /**
  *
+ * Returns the relative time from date
+ *
+ * @param {!Date} date - The date to compare relative time
+ * @param {Locale} [locale='en-US'] - The locale to return string, by default is `'en-US'`
+ * @returns {string} The relative time on locale format
+ *
+ * @example
+ *
+ * // if current date is new Date(1996, 05, 26)
+ *
+ * const myDate = new Date(1996, 05, 25)
+ * console.log(relativeTime(myDate)) // 'yesterday'
  */
 export function relativeTime(date: Date, locale: Locale = 'en-US'): string {
   return new RelativeTimeFunction(new RelativeTimeOwn()).relativeTime(date, locale);
