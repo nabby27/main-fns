6cc388b937ced65e5619d9037198720007d0e85b
nabby27 <ivancordoba77@gmail.com>
2021-01-18 09:31:50 +0100

    change implementation

--- src/app/math/max/impl/maxOwn.ts
+++ src/app/math/max/impl/maxOwn.ts
@@ -3,7 +3,7 @@ import { Max } from '../util/max.interface';
 export class MaxOwn implements Max {
 
   max(numbers: number[]): number {
-    return numbers.reduce((acc: number, number: number) => number > acc ? number : acc);
+    return Math.max(...numbers);
   }
 
 }
--- src/app/math/min/impl/minOwn.ts
+++ src/app/math/min/impl/minOwn.ts
@@ -3,7 +3,7 @@ import { Min } from '../util/min.interface';
 export class MinOwn implements Min {
 
   min(numbers: number[]): number {
-    return numbers.reduce((acc: number, number: number) => acc > number ? number : acc);
+    return Math.min(...numbers);
   }
 
 }
